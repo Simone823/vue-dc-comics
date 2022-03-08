47af7088c5be25755a1084d0d6da2b167e0dedea
Simone823 <simone.daglio823@outlook.it>
2022-03-09 00:58:29 +0100

    footer nav

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <SectionBlack/>
     <SectionBlue/>
     <Footer/>
+    <FooterNav/>
   </div>
 </template>
 
@@ -13,6 +14,7 @@ import Header from "./components/Header.vue"
 import SectionBlack from "./components/SectionBlack.vue"
 import SectionBlue from "./components/SectionBlue.vue"
 import Footer from "./components/Footer.vue"
+import FooterNav from "./components/FooterNav.vue"
 
 export default {
   name: 'App',
@@ -21,6 +23,7 @@ export default {
     SectionBlack,
     SectionBlue,
     Footer,
+    FooterNav,
   }
 }
 </script>
---
+++ src/components/FooterNav.vue
@@ -0,0 +1,107 @@
+<template>
+    <!-- Footer nav -->
+    <nav>
+        <div class="container">
+            <!-- Button -->
+            <div class="button">
+                <button>Sign-Up Now!</button>
+            </div>
+
+            <!-- Social -->
+            <div class="social">
+                <h2>{{titleSocial}}</h2>
+                <ul>
+                    <li v-for="(element, index) in arrayIconSocial" :key="index">
+                        <a href="element.href"><img :src="require(`../assets${element.icon}`)" alt=""></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+
+<script>
+export default {
+    name: "FooterNav",
+    data() {
+        return {
+            // Titolo div social
+            titleSocial: "Follow Us",
+
+            // Array Link e icone social
+            arrayIconSocial: [
+                {
+                    href: "#",
+                    icon: "/img/footer-facebook.png",
+                },
+
+                {
+                    href: "#",
+                    icon: "/img/footer-twitter.png",
+                },
+
+                {
+                    href: "#",
+                    icon: "/img/footer-youtube.png",
+                },
+
+                {
+                    href: "#",
+                    icon: "/img/footer-pinterest.png",
+                },
+
+                {
+                    href: "#",
+                    icon: "/img/footer-periscope.png",
+                },
+            ], 
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+nav {
+    background-color: #303030;
+    padding: 25px 20px;
+
+    .container {
+        max-width: 1400px;
+        margin: 0 auto;
+        // border: 2px solid red;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        row-gap: 20px;
+
+        button {
+            padding: 15px;
+            text-transform: uppercase;
+            background-color: transparent;
+            color: white;
+            border: 2px solid #0282f9;
+            cursor: pointer;
+        }
+
+        .social {
+            display: flex;
+            align-items: center;
+            gap: 30px;
+            flex-wrap: wrap;
+
+            h2 {
+                color: #0282f9;
+                text-transform: uppercase;
+            }
+
+            ul {
+                list-style: none;
+                display: flex;
+                gap: 15px;
+            }
+        }
+    }
+}
+</style>
