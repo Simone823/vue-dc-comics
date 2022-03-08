b3a5ee250f4f2f42f0fed26e22b71268bb152eb4
Simone823 <simone.daglio823@outlook.it>
2022-03-08 16:43:03 +0100

    header

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
   <div id="app">
-
+    <Header/>
   </div>
 </template>
 
 <script>
 
+import Header from "./components/Header.vue"
 
 export default {
   name: 'App',
   components: {
-
+    Header,
   }
 }
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Header.vue
@@ -0,0 +1,139 @@
+// HTML
+<template>
+
+    <!-- Header -->
+    <header>
+        <!-- Container -->
+        <div class="container">
+            <!-- Logo  -->
+            <figure class="logo">
+                <img src="..\src\assets\img\dc-logo.png" alt="">
+            </figure>
+            <!-- Nav -->
+            <nav class="menu">
+                <ul>
+                    <li v-for="(element, index) in menuLinks" :key="index">
+                        <a :href="element.href">{{element.text}}</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
+    </header>
+
+
+</template>
+
+
+
+// Javascript
+<script>
+export default {
+    name: "MyHeader",
+
+    data() {
+        return {
+
+            // Nav Links
+            menuLinks: [
+                {
+                    href: "#",
+                    text: "Characters", 
+                },
+
+                {
+                    href: "#",
+                    text: "Comics", 
+                },
+
+                {
+                    href: "#",
+                    text: "Movies", 
+                },
+
+                {
+                    href: "#",
+                    text: "Tv", 
+                },
+
+                {
+                    href: "#",
+                    text: "Games", 
+                },
+
+                {
+                    href: "#",
+                    text: "Collectibles", 
+                },
+
+                {
+                    href: "#",
+                    text: "Videos", 
+                },
+
+                {
+                    href: "#",
+                    text: "Fan", 
+                },
+
+                {
+                    href: "#",
+                    text: "News", 
+                },
+
+                {
+                    href: "#",
+                    text: "Shop", 
+                },
+            ],
+        }
+    },
+}
+</script>
+
+
+// Stile scss valido solo per questo componenete
+<style lang="scss" scoped>
+header {
+    padding: 5px 20px;
+
+    .container {
+        max-width: 1400px;
+        margin: 0 auto;
+        // border: 2px solid red;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        row-gap: 20px;
+
+        .logo {
+            width: 82px;
+            height: 82px;
+
+
+            img {
+                display: block;
+                width: 100%;
+            }
+        }
+
+        .menu ul {
+            list-style: none;
+            display: flex;
+            gap: 20px;
+            flex-wrap: wrap;
+            
+
+            a {
+                text-decoration: none;
+                color: currentColor;
+                text-transform: uppercase;
+                font-weight: 600;
+            }
+        }
+
+    }
+}
+
+</style>
