6b702aa78f3f55aa1cff680b512b01756484356d
Simone823 <simone.daglio823@outlook.it>
2022-03-09 15:40:48 +0100

    jumbotron sectionBlack

--- src/components/SectionBlack.vue
+++ src/components/SectionBlack.vue
@@ -1,13 +1,18 @@
 <template>
+    <main>
+        <!-- Jumbotron -->
+        <section class="jumbotron">
+            <img src="../assets/img/jumbotron.jpg" alt="">
+        </section>
 
-    <!-- Contenuto parte centrale sfondo nero -->
-    <section class="bkg-black">
-        <!-- Container -->
-        <div class="container">
-            <h2>--> Content goes here</h2>
-        </div>   
-    </section>
-
+        <!-- Contenuto parte centrale sfondo nero -->
+        <section class="bkg-black">
+            <!-- Container -->
+            <div class="container">
+                <h2>--> Content goes here</h2>
+            </div>   
+        </section>
+    </main>
 </template>
 
 
@@ -22,9 +27,22 @@ export default {
 
 <style lang="scss" scoped>
 
+.jumbotron {
+    width: 100%;
+    height: 390px;
+
+    img {
+        width: 100%;
+        height: 100%;
+        display: block;
+        object-fit: cover;
+        object-position: top center;
+    }
+}
+
 .bkg-black {
     width: 100%;
-    padding: 50px 20px;
+    // padding: 50px 20px;
     background-color: #1c1c1c;
     color: white;
 
