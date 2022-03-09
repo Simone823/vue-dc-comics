a9de7f900ac5d79e669d0101f5558b8cfc8f87bf
Simone823 <simone.daglio823@outlook.it>
2022-03-09 16:01:57 +0100

    modifica jumbotron e aggiunta button jumbotron

--- src/components/SectionBlack.vue
+++ src/components/SectionBlack.vue
@@ -2,7 +2,9 @@
     <main>
         <!-- Jumbotron -->
         <section class="jumbotron">
-            <img src="../assets/img/jumbotron.jpg" alt="">
+            <div class="container">
+                <button>Current Series</button>
+            </div>
         </section>
 
         <!-- Contenuto parte centrale sfondo nero -->
@@ -30,19 +32,32 @@ export default {
 .jumbotron {
     width: 100%;
     height: 390px;
+    background-image: url("../assets/img/jumbotron.jpg");
+    background-repeat: no-repeat;
+    background-position: top center;
+    position: relative;
+    padding: 0 20px;
 
-    img {
-        width: 100%;
-        height: 100%;
-        display: block;
-        object-fit: cover;
-        object-position: top center;
+    .container {
+        max-width: 1400px;
+        margin: 0 auto;
+
+        button {
+            padding: 10px 15px;
+            color: white;
+            background-color: #0282f9;
+            border: none;
+            position: absolute;
+            bottom: 0;
+            transform: translateY(50%);
+            text-transform: uppercase;
+        }
     }
 }
 
 .bkg-black {
     width: 100%;
-    // padding: 50px 20px;
+    padding: 50px 20px;
     background-color: #1c1c1c;
     color: white;
 
